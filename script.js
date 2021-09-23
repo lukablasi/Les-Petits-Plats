e1c6ea73a6ed873ecd9480b2391875d951d6ee8e
lukablasi <lukablasi@gmail.com>
2021-09-23 17:55:38 +0100

    most functionalities created

--- script.js
+++ script.js
@@ -51,6 +51,15 @@ async function populateRecipes(filter) {
         recipeContainer.classList.add('col-md-6');
         recipeContainer.classList.add('col-lg-4');
         recipeContainer.classList.add('my-3');
+        recipeContainer.classList.add('recipe-container');
+        recipeContainer.classList.add(recipe.appliance.split(' ').join('-').toLowerCase())
+        for (let ustensil of recipe.ustensils) {
+            recipeContainer.classList.add(ustensil.split(' ').join('-').toLowerCase())
+        }
+        for (let ingredients of recipe.ingredients) {
+            recipeContainer.classList.add(ingredients.ingredient.split(' ').join('-').toLowerCase())
+        }
+        
         
         recipePlaceholder.setAttribute('src', 'placeholder.png');
         recipePlaceholder.classList.add('w-100');
@@ -122,3 +131,334 @@ function removeAllChildNodes(parent) {
         parent.removeChild(parent.firstChild);
     }
 }
+
+//create ingredients box
+const ingredientsSelectContainer = document.getElementById('ingredients-container');
+const ingredientsSelectBox = document.createElement('div');
+ingredientsSelectBox.classList.add('dropdown-menu');
+ingredientsSelectBox.classList.add('ingredients-menu');
+ingredientsSelectBox.setAttribute('aria-labelledby', 'ingredients-container');
+
+
+ingredientsSelectContainer.appendChild(ingredientsSelectBox);
+
+const ingredientsContainer = document.getElementById('ingredients-container');
+const ingredientsButton = document.getElementById('ingredients-button');
+    
+let allIngredientsBox = [];
+
+for (let recipe of recipes) {
+    for (let ingredient of recipe.ingredients) {
+        allIngredientsBox.push(ingredient.ingredient)
+    }
+}
+let uniqueIngredients = [...new Set(allIngredientsBox)];
+uniqueIngredients.forEach(function(ingredient) {
+    const ingredientsInstance = document.createElement('a');
+    ingredientsSelectBox.appendChild(ingredientsInstance).innerHTML = ingredient;
+    ingredientsInstance.classList.add('dropdown-item');
+    let ingredientClass = ingredientsInstance.innerHTML.toLowerCase().split(' ');
+    ingredientsInstance.classList.add('ingredient');
+    for (let clas of ingredientClass) {
+        ingredientsInstance.classList.add(clas);
+    }
+    
+    ingredientsInstance.setAttribute('href', '#')
+    ingredientsInstance.addEventListener('click', () => {
+        const selectedTags = document.getElementById('selected-tags');
+        const selectedTag = document.createElement('button');
+        selectedTags.appendChild(selectedTag);
+        selectedTag.classList.add('btn');
+        selectedTag.classList.add('btn-primary');
+        selectedTag.classList.add('col-2');
+        selectedTag.innerHTML = ingredientsInstance.innerHTML;
+        addTag();
+        selectedTag.addEventListener('click', () => {
+            selectedTags.removeChild(selectedTag);
+            addTag()
+        })
+    })
+});
+
+//ingredients button handler
+const ingredientsInput = document.getElementById('ingredients-input');
+ingredientsInput.addEventListener('click', () => {
+    ingredientsSelectBox.classList.add('show');
+})
+
+ingredientsInput.addEventListener('blur', (e) => {
+    ingredientsSelectBox.classList.remove('show');
+    e.target.value = '';
+})
+
+const lowerUniqueIngredients = uniqueIngredients.map(v => v.toLowerCase());
+
+ingredientsInput.addEventListener('keyup', (e) => {
+    if (e.target.value.length > 2) {
+        let filter = e.target.value.toLowerCase();
+        let filteredIngredients = lowerUniqueIngredients.filter(ingri => {
+            return ingri.includes(filter)
+        });
+        let ingredientsCollection = document.querySelectorAll('.ingredient');
+        
+        for (let ingredient of ingredientsCollection) {
+            ingredient.classList.add('d-none');
+        }
+        for (let filteredIngredient of filteredIngredients) {
+            const instances = document.getElementsByClassName(filteredIngredient);
+            for (let instance of instances) {
+                instance.classList.remove('d-none');
+                instance.classList.add('d-block');
+            }
+        }
+    } else {
+            const instances = document.getElementsByClassName('ingredient');
+            for (let instance of instances) {
+                instance.classList.remove('d-none');
+                instance.classList.add('d-block');
+            }
+    }
+})
+
+//create device box
+let allDevices = [];
+
+for (recipe of recipes) {
+    allDevices.push(recipe.appliance)
+}
+
+let uniqueDevices = [...new Set(allDevices)];
+
+const deviceContainer = document.getElementById('device-container');
+const deviceMenu = document.createElement('div');
+deviceContainer.appendChild(deviceMenu);
+deviceMenu.classList.add('dropdown-menu');
+deviceMenu.classList.add('device-menu');
+
+uniqueDevices.forEach(function(device) {
+    const deviceInstance = document.createElement('a');
+    deviceMenu.appendChild(deviceInstance).innerHTML = device;
+    deviceInstance.classList.add('dropdown-item');
+    const deviceClass = deviceInstance.innerHTML.toLowerCase().split(' ');
+    deviceInstance.classList.add('device');
+    for (let clas of deviceClass) {
+        deviceInstance.classList.add(clas);
+    }
+    deviceInstance.setAttribute('href', '#');
+    deviceInstance.addEventListener('click', () => {
+        const selectedTags = document.getElementById('selected-tags');
+        const selectedTag = document.createElement('button');
+        selectedTags.appendChild(selectedTag);
+        selectedTag.classList.add('btn');
+        selectedTag.classList.add('btn-success');
+        selectedTag.classList.add('col-2');
+        selectedTag.innerHTML = deviceInstance.innerHTML;
+        addTag();
+        selectedTag.addEventListener('click', () => {
+            selectedTags.removeChild(selectedTag);
+            addTag()
+        })
+    })
+});
+
+//devices button handler
+const devicesInput = document.getElementById('device-input');
+devicesInput.addEventListener('click', () => {
+    deviceMenu.classList.add('show');
+})
+
+devicesInput.addEventListener('blur', (e) => {
+    deviceMenu.classList.remove('show');
+    e.target.value = '';
+})
+
+const lowerUniqueDevices = uniqueDevices.map(v => v.toLowerCase());
+
+devicesInput.addEventListener('keyup', (e) => {
+    if (e.target.value.length > 2) {
+        let filter = e.target.value.toLowerCase();
+        let filteredDevices = lowerUniqueDevices.filter(dev=> {
+            return dev.includes(filter)
+        });
+        let devicesCollection = document.querySelectorAll('.device');
+        for (let device of devicesCollection) {
+            device.classList.add('d-none');
+        }
+        for (let filteredDevice of filteredDevices) {
+            const instances = document.getElementsByClassName(filteredDevice);
+            for (let instance of instances) {
+                instance.classList.remove('d-none');
+                instance.classList.add('d-block');
+                
+            }
+        }
+    } else {
+            const instances = document.getElementsByClassName('device');
+            for (let instance of instances) {
+                instance.classList.remove('d-none');
+                instance.classList.add('d-block');
+            }
+    }
+})
+
+//create ustensils box
+let allUstensils = [];
+
+for (let recipe of recipes) {
+    for (let ustensil of recipe.ustensils) {
+        allUstensils.push(ustensil)
+    }
+}
+let uniqueUstensils = [...new Set(allUstensils)];
+
+const ustensilContainer = document.getElementById('ustensils-container');
+const ustensilMenu = document.createElement('div');
+ustensilContainer.appendChild(ustensilMenu);
+ustensilMenu.classList.add('dropdown-menu');
+ustensilMenu.classList.add('ustensil-menu');
+
+uniqueUstensils.forEach(function(ustensil) {
+    const ustensilInstance = document.createElement('a');
+    ustensilMenu.appendChild(ustensilInstance).innerHTML = ustensil;
+    ustensilInstance.classList.add('dropdown-item');
+    const ustensilClass = ustensilInstance.innerHTML.toLowerCase().split(' ');
+    ustensilInstance.classList.add('ustensil');
+    for (let clas of ustensilClass) {
+        ustensilInstance.classList.add(clas);
+    }
+    ustensilInstance.setAttribute('href', '#');
+    ustensilInstance.addEventListener('click', () => {
+        const selectedTags = document.getElementById('selected-tags');
+        const selectedTag = document.createElement('button');
+        selectedTags.appendChild(selectedTag);
+        selectedTag.classList.add('btn');
+        selectedTag.classList.add('btn-danger');
+        selectedTag.classList.add('col-2');
+        selectedTag.innerHTML = ustensilInstance.innerHTML;
+        addTag();
+        selectedTag.addEventListener('click', () => {
+            selectedTags.removeChild(selectedTag);
+            addTag()
+        })
+    })
+});
+
+//ustensils button handler
+const ustensilsInput = document.getElementById('ustensils-input');
+ustensilsInput.addEventListener('click', () => {
+    ustensilMenu.classList.add('show');
+})
+
+ustensilsInput.addEventListener('blur', (e) => {
+    ustensilMenu.classList.remove('show');
+    e.target.value = '';
+})
+
+const lowerUniqueUstensils = uniqueUstensils.map(v => v.toLowerCase());
+
+ustensilsInput.addEventListener('keyup', (e) => {
+    if (e.target.value.length > 2) {
+        let filter = e.target.value.toLowerCase();
+        let filteredUstensils = lowerUniqueUstensils.filter(dev=> {
+            return dev.includes(filter)
+        });
+        let ustensilsCollection = document.querySelectorAll('.ustensil');
+        for (let ustensil of ustensilsCollection) {
+            ustensil.classList.add('d-none');
+        }
+        for (let filteredUstensil of filteredUstensils) {
+            const instances = document.getElementsByClassName(filteredUstensil);
+            for (let instance of instances) {
+                instance.classList.remove('d-none');
+                instance.classList.add('d-block');
+                
+            }
+        }
+    } else {
+            const instances = document.getElementsByClassName('ustensil');
+            for (let instance of instances) {
+                instance.classList.remove('d-none');
+                instance.classList.add('d-block');
+            }
+    }
+})
+
+//filter by selected tags
+// let tags = [];
+function addTag() {
+//     let tag = selectedTag.innerHTML.split(' ').join('-').toLowerCase();
+//     currentTags = 
+//     const allRecipes = document.getElementsByClassName('recipe-container');
+//     for (let recipe of allRecipes) {
+        
+//         if (recipe.classList.contains(tag)) {
+            
+//         } else {
+//             recipe.classList.add('d-none')
+//         }
+    
+// }
+    const allRecipes = document.getElementsByClassName('recipe-container');
+    const selectedTags = document.getElementById('selected-tags').children;
+
+    for (let recipe of allRecipes) {
+        for (let tag of selectedTags) {
+            tag = tag.innerHTML.split(' ').join('-').toLowerCase();
+            console.log(tag)
+            if (recipe.classList.contains(tag)) {
+                            recipe.classList.remove('d-none')
+                            recipe.classList.add('d-block')
+                            
+                        } else {
+                            recipe.classList.remove('d-block')
+                            recipe.classList.add('d-none')
+                            
+                        }
+        }
+    }
+    
+    // for (let tag of selectedTags) {
+    //     tag = tag.innerHTML.split(' ').join('-').toLowerCase();
+    //     console.log(tag)
+    //     for (let recipe of allRecipes) {
+            
+    //         if (recipe.classList.contains(tag)) {
+    //             recipe.classList.remove('d-none')
+    //             recipe.classList.add('d-block')
+                
+    //         } else {
+    //             recipe.classList.remove('d-block')
+    //             recipe.classList.add('d-none')
+                
+    //         }
+    //     }
+    // }
+}
+
+// function removeTag(selectedTag) {
+    
+//     let tag = selectedTag.innerHTML.split(' ').join('-').toLowerCase();
+//     const allRecipes = document.getElementsByClassName('recipe-container');
+//     for (let recipe of allRecipes) {
+        
+//             if (recipe.classList.contains(tag)) {
+                
+//             } else {
+//                 recipe.classList.remove('d-none')
+//             }
+        
+//     }
+// }
+
+// function updateTags(tags) {
+//     const allRecipes = document.getElementsByClassName('recipe-container');
+//     for (let recipe of allRecipes) {
+//         for (let tag of tags) {
+//             if (recipe.classList.contains(tag)) {
+//                 recipe.classList.add('d-block')
+//             } else {
+//                 recipe.classList.add('d-none')
+//             }
+//         }
+//     }
+// }
